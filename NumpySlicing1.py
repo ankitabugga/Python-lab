60dfadddfed8308020434132a1acbb8f15f7e744
ankitabugga <ankitabugga@gmail.com>
2024-07-22 16:16:00 +0530

    Add files via upload

---
+++ NumpySlicing1.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# Create an array of 10 zeros
+zeros = np.zeros(10)
+print(zeros)
+# Create an array of 10 ones
+ones = np.ones(10)
+print(ones)
+# Create an array of 10 fives
+fives = np.full(10, 5)
+print(fives)
+# Concatenate the arrays
+#result = np.concatenate((zeros, ones, fives))
+#print(result)
+
+#output
+
+
+'''[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+[1. 1. 1. 1. 1. 1. 1. 1. 1. 1.]
+[5 5 5 5 5 5 5 5 5 5]'''
