77bd3ce0040d0d0c55176d993426a3364cfe8be1
ankitabugga <ankitabugga@gmail.com>
2024-07-22 16:17:18 +0530

    Add files via upload

---
+++ numpy1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Define the list
+my_list = [1, 2, 3, 4, 5]
+
+# Convert the list to a NumPy array
+my_array = np.array(my_list)
+
+# Display the NumPy array
+print(my_array)
+
+
+#output
+#[1 2 3 4 5]
