6b5b9bf989fc511537b5987e6fc5c2859953b088
ankitabugga <ankitabugga@gmail.com>
2024-07-24 16:21:50 +0530

    Add files via upload

---
+++ assignmentsd.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Given array
+arr = np.array([20, 2, 7, 1, 34])
+
+# Compute the standard deviation
+std_dev = np.std(arr)
+
+# Print the result
+print("The standard deviation is:", std_dev)
+
+#output
+#The standard deviation is: 12.576167937809991
