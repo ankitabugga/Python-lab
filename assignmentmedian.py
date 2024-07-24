3625a97ecddb6fb2ffe202d565fde4c53dfb2f5d
ankitabugga <ankitabugga@gmail.com>
2024-07-24 16:21:31 +0530

    Add files via upload

---
+++ assignmentmedian.py
@@ -0,0 +1,20 @@
+"""Compute the median of the flattened NumPy array 
+'
+   Input: x_odd = np.array([1, 2, 3, 4, 5, 6, 7])"""
+
+
+
+import numpy as np
+
+# Given array
+x_odd = np.array([1, 2, 3, 4, 5, 6, 7])
+
+# Compute the median
+median = np.median(x_odd)
+
+# Print the result
+print("The median is:", median)
+
+
+#output:
+#The median is: 4.0
