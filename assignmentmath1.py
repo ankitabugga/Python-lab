ea0fc17eba479d2924c3e7d6e34b0e12cb61f3a2
ankitabugga <ankitabugga@gmail.com>
2024-07-26 08:47:11 +0530

    Add files via upload

---
+++ assignmentmath1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+category1_revenue = np.array([500, 600, 700, 550])
+category2_revenue = np.array([450, 700, 800, 600])
+
+total_revenue = category1_revenue + category2_revenue
+print("Total Revenue:", total_revenue)
+
+
+#output
+#Total Revenue: [ 950 1300 1500 1150]
